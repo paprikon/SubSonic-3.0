8a3c7359df90c47104830c22e6295ea658719a8a
Nathan Bridgewater <nathan@integratedwebsystems.com>
2010-08-30 11:11:04 -0500

    more placeholders missed in first commit

---
+++ SubSonic.Core/DataProviders/PostgreSql/PostgreSqlProvider.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using SubSonic.Schema;
+using System.Data.Common;
+using SubSonic.Linq.Structure;
+using SubSonic.Query;
+
+
+namespace SubSonic.DataProviders.PostgreSql
+{
+
+    public class PostgreSqlProvider : DbDataProvider, IDataProvider
+    {
+        public override string InsertionIdentityFetchString { get { return String.Empty; } }
+
+		  public PostgreSqlProvider(string connectionString, string providerName)
+			  : base(connectionString, providerName)
+        {}
+
+        public override string QualifyTableName(ITable table)
+        {
+            string qualifiedTable;
+
+            qualifiedTable = qualifiedTable = String.Format("`{0}`", table.Name);
+
+
+            return qualifiedTable;
+        }
+
+        public override string QualifyColumnName(IColumn column)
+        {
+            string qualifiedFormat;
+            qualifiedFormat = "`{2}`";
+            return String.Format(qualifiedFormat, column.Table.SchemaName, column.Table.Name, column.Name);
+        }
+
+        public override ISchemaGenerator SchemaGenerator
+        {
+            get { return new PostgreSqlSchema(); }
+        }
+
+        public override ISqlGenerator GetSqlGenerator(SqlQuery query)
+        {
+            return new PostgreSqlGenerator(query);
+        }
+
+        public override IQueryLanguage QueryLanguage { get { return new PostgreSqlLanguage(this); } }
+
+    }
+}
---
+++ SubSonic.Tests/Repositories/PostgreSqlSimpleRepositoryTests.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using SubSonic.DataProviders;
+
+namespace SubSonic.Tests.Repositories
+{
+    public class PostgreSqlSimpleRepositoryTests : SimpleRepositoryTests
+    {
+		 public PostgreSqlSimpleRepositoryTests() :
+			 base(ProviderFactory.GetProvider(@"host=localhost;database=SubSonic;user id=subsonic; password=pass;", "NpSql.PostgreSqlCient"))
+        {
+        }
+    }
+}
