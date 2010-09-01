b289f2970aa873208a73d3cf473088c320d0e47f
Nathan Bridgewater <nathan@integratedwebsystems.com>
2010-08-31 23:27:56 -0500

    18/20 pass/fail. Enum parameter is hanging up on eval in the postgresql ado.net provider. Fixed a few basic items for these 18 to pass.

--- SubSonic.Core/DataProviders/PostgreSql/PostgreSqlSchema.cs
+++ SubSonic.Core/DataProviders/PostgreSql/PostgreSqlSchema.cs
@@ -16,238 +16,253 @@ using System.Data;
 using System.Text;
 using SubSonic.Schema;
 using SubSonic.SqlGeneration.Schema;
+using SubSonic.Extensions;
 
 
 namespace SubSonic.DataProviders.PostgreSql
 {
 
-    public class PostgreSqlSchema : ANSISchemaGenerator
-    {
-        public PostgreSqlSchema()
-        {
-            ADD_COLUMN = "ALTER TABLE \"{0}\" ADD \"{1}\"{2};";
-            //can't do this
-            ALTER_COLUMN = @"";
-            CREATE_TABLE = "CREATE TABLE \"{0}\" ({1} \r\n);";
-            //can't do this
-            DROP_COLUMN = @"";
-            DROP_TABLE = "DROP TABLE \"{0}\";";
+	public class PostgreSqlSchema : ANSISchemaGenerator
+	{
+		public PostgreSqlSchema()
+		{
+			ADD_COLUMN = "ALTER TABLE \"{0}\" ADD \"{1}\"{2};";
+			//can't do this
+			ALTER_COLUMN = @"";
+			CREATE_TABLE = "CREATE TABLE \"{0}\" ({1} \r\n);";
+			//can't do this
+			DROP_COLUMN = @"";
+			DROP_TABLE = "DROP TABLE \"{0}\";";
 
-            UPDATE_DEFAULTS = "UPDATE \"{0}\" SET \"{1}\"={2};";
+			UPDATE_DEFAULTS = "UPDATE \"{0}\" SET \"{1}\"={2};";
 
-            ClientName = "Npgsql.PostgreSqlClient";
-        }
+			ClientName = "Npgsql.PostgreSqlClient";
+		}
 
-        public override string GetNativeType(DbType dbType)
-        {
-            switch(dbType)
-            {
-                case DbType.Object:
-                case DbType.AnsiString:
-                case DbType.AnsiStringFixedLength:
-                case DbType.String:
-                case DbType.StringFixedLength:
-                    return "varchar";
-                case DbType.Boolean:
-                    return "tinyint";
-                case DbType.SByte:
-                case DbType.Binary:
-                case DbType.Byte:
-                    return "blob";
-                case DbType.Currency:
-                    return "money";
-                case DbType.Time:
-                case DbType.Date:
-                case DbType.DateTime:
-                    return "datetime";
-                case DbType.Decimal:
-                    return "decimal";
-                case DbType.Double:
-                    return "float";
-                case DbType.Guid:
-                    return "guid";
-                case DbType.UInt32:
-                case DbType.Int32:
-                    return "int";
-                case DbType.Int16:
-                case DbType.UInt16:
-                    return "tinyint";
-                case DbType.UInt64:
-                case DbType.Int64:
-                    return "integer";
-                case DbType.Single:
-                    return "real";
-                case DbType.VarNumeric:
-                    return "numeric";
-                case DbType.Xml:
-                    return "xml";
-                default:
-                    return "varchar";
-            }
-        }
+		public override string GetNativeType(DbType dbType)
+		{
+			switch (dbType)
+			{
+				case DbType.Object:
+				case DbType.AnsiString:
+				case DbType.AnsiStringFixedLength:
+				case DbType.String:
+				case DbType.StringFixedLength:
+					return "varchar";
+				case DbType.Boolean:
+					return "boolean";
+				case DbType.SByte:
+				case DbType.Binary:
+				case DbType.Byte:
+					return "bytea";
+				case DbType.Currency:
+					return "money";
+				case DbType.Time:
+				case DbType.Date:
+				case DbType.DateTime:
+					return "timestamp";
+				case DbType.Decimal:
+					return "decimal";
+				case DbType.Double:
+					return "real";
+				case DbType.Guid:
+					return "uuid";
+				case DbType.UInt32:
+				case DbType.Int32:
+					return "integer";
+				case DbType.Int16:
+				case DbType.UInt16:
+					return "smallint";
+				case DbType.UInt64:
+				case DbType.Int64:
+					return "bigint";
+				case DbType.Single:
+					return "smallint";
+				case DbType.VarNumeric:
+					return "numeric";
+				case DbType.Xml:
+					return "xml";
+				default:
+					return "varchar";
+			}
+		}
 
-        public override string BuildDropColumnStatement(string tableName, string columnName)
-        {
-			  //TODO: drop column
-            return "";
-        }
+		public override string BuildDropColumnStatement(string tableName, string columnName)
+		{
+			//TODO: drop column
+			return "";
+		}
 
-        public override string BuildAlterColumnStatement(IColumn column)
-        {
-			  //TODO: alter column
-            return "";
-        }
+		public override string BuildAlterColumnStatement(IColumn column)
+		{
+			//TODO: alter column
+			return "";
+		}
 
-        /// <summary>
-        /// Sets the column attributes.
-        /// </summary>
-        /// <param name="column">The column.</param>
-        /// <returns></returns>
-        public override string GenerateColumnAttributes(IColumn column)
-        {
-            StringBuilder sb = new StringBuilder();
-            if (column.DataType == DbType.String && column.MaxLength > 8000)
-                sb.Append(" TEXT ");
-            else if (column.IsPrimaryKey && column.DataType == DbType.Int32
-                || column.IsPrimaryKey && column.DataType == DbType.Int16
-                || column.IsPrimaryKey && column.DataType == DbType.Int64
-                )
-                sb.Append(" integer ");
-            else
-                sb.Append(" " + GetNativeType(column.DataType));
+		/// <summary>
+		/// Sets the column attributes.
+		/// </summary>
+		/// <param name="column">The column.</param>
+		/// <returns></returns>
+		public override string GenerateColumnAttributes(IColumn column)
+		{
+			StringBuilder sb = new StringBuilder();
+			if (column.DataType == DbType.String && column.MaxLength > 8000)
+				sb.Append(" TEXT ");
+			else if (column.IsPrimaryKey && column.DataType == DbType.Int32
+				 || column.IsPrimaryKey && column.DataType == DbType.Int16
+				 || column.IsPrimaryKey && column.DataType == DbType.Int64
+				 )
+			{
+				if (column.AutoIncrement)
+					sb.Append(" SERIAL");
+				else
+					sb.Append(" INTEGER ");
+			}
+			else
+				sb.Append(" " + GetNativeType(column.DataType));
 
-            if (column.IsString && column.MaxLength < 8000)
-                sb.Append("(" + column.MaxLength + ")");
-            else if (column.DataType == DbType.Double || column.DataType == DbType.Decimal)
-                sb.Append("(" + column.NumericPrecision + ", " + column.NumberScale + ")");
+			if (column.IsString && column.MaxLength < 8000)
+				sb.Append("(" + column.MaxLength + ")");
+			else if (column.DataType == DbType.Double || column.DataType == DbType.Decimal)
+				sb.Append("(" + column.NumericPrecision + ", " + column.NumberScale + ")");
 
-            if(column.IsPrimaryKey)
-            {
-                sb.Append(" NOT NULL PRIMARY KEY");
-                if(column.IsNumeric && column.AutoIncrement)
-                    sb.Append(" AUTOINCREMENT ");
-            }
-            else
-            {
-                if(!column.IsNullable)
-                    sb.Append(" NOT NULL");
-                else
-                    sb.Append(" NULL");
+			if (column.IsPrimaryKey)
+			{
+				sb.Append(" NOT NULL");
+			}
+			else
+			{
+				if (!column.IsNullable)
+					sb.Append(" NOT NULL");
+				else
+					sb.Append(" NULL");
 
-                if(column.DefaultSetting != null)
-                    sb.Append(" DEFAULT '" + column.DefaultSetting + "'");
-            }
+				if (column.DefaultSetting != null)
+					sb.Append(" DEFAULT '" + column.DefaultSetting + "'");
+			}
 
-            return sb.ToString();
-        }
+			return sb.ToString();
+		}
 
-        /// <summary>
-        /// Gets the type of the db.
-        /// </summary>
-        /// <param name="sqlType">Type of the SQL.</param>
-        /// <returns></returns>
-        public override DbType GetDbType(string sqlType)
-        {
-            switch (sqlType.ToLowerInvariant())
-            {
-                case "longtext":
-                case "nchar":
-                case "ntext":
-                case "text":
-                case "sysname":
-                case "varchar":
-                case "nvarchar":
-                    return DbType.String;
-                case "bit":
-                case "tinyint":
-                    return DbType.Boolean;
-                case "decimal":
-                case "float":
-                case "newdecimal":
-                case "numeric":
-                case "double":
-                case "real":
-                    return DbType.Decimal;
-                case "int":
-                case "int32":
-                    return DbType.Int32;
-                case "integer":
-                    return DbType.Int64;
-                case "int16":
-                case "smallint":
-                    return DbType.Int16;
-                case "date":
-                case "time":
-                case "datetime":
-                case "smalldatetime":
-                    return DbType.DateTime;
-                case "image":
-                case "varbinary":
-                case "binary":
-                case "blob":
-                case "longblob":
-                    return DbType.Binary;
-                case "char":
-                    return DbType.AnsiStringFixedLength;
-                case "currency":
-                case "money":
-                case "smallmoney":
-                    return DbType.Currency;
-                case "timestamp":
-                    return DbType.DateTime;
-                case "uniqueidentifier":
-                    return DbType.Guid;
-                case "uint16":
-                    return DbType.UInt16;
-                case "uint32":
-                    return DbType.UInt32;
-                default:
-                    return DbType.String;
-            }
-        }
+		/// <summary>
+		/// Gets the type of the db.
+		/// </summary>
+		/// <param name="sqlType">Type of the SQL.</param>
+		/// <returns></returns>
+		public override DbType GetDbType(string sqlType)
+		{
+			switch (sqlType.ToLowerInvariant())
+			{
+				case "longtext":
+				case "nchar":
+				case "ntext":
+				case "text":
+				case "sysname":
+				case "varchar":
+				case "nvarchar":
+					return DbType.String;
+				case "bit":
+				case "boolean":
+					return DbType.Boolean;
+				case "decimal":
+				case "newdecimal":
+				case "numeric":
+				case "double":
+				case "real":
+					return DbType.Decimal;
+				case "int":
+				case "int4" :
+				case "integer" :
+					return DbType.Int32;
+				case "int8" :
+				case "bigint":
+					return DbType.Int64;
+				case "int2" :
+				case "smallint":
+					return DbType.Int16;
+				case "date":
+				case "time":
+				case "timestamp":
+					return DbType.DateTime;
+				case "lo":
+				case "bytea" :
+					return DbType.Binary;
+				case "char":
+					return DbType.AnsiStringFixedLength;
+				case "money":
+					return DbType.Currency;
+				case "uuid":
+					return DbType.Guid;
+				default:
+					return DbType.String;
+			}
+		}
 
 
-        public override void SetColumnDefaults(IColumn column) {
-            if (column.IsNumeric)
-                column.DefaultSetting = 0;
-            else if (column.IsDateTime)
-                column.DefaultSetting = "1900-01-01";
-            else if (column.IsString)
-                column.DefaultSetting = "";
-            else if (column.DataType == DbType.Boolean)
-                column.DefaultSetting = false;
-        }
+		public override void SetColumnDefaults(IColumn column)
+		{
+			if (column.IsNumeric)
+				column.DefaultSetting = 0;
+			else if (column.IsDateTime)
+				column.DefaultSetting = "1900-01-01";
+			else if (column.IsString)
+				column.DefaultSetting = "";
+			else if (column.DataType == DbType.Boolean)
+				column.DefaultSetting = false;
+		}
 
-		  public override ITable GetTableFromDB(IDataProvider provider, string tableName)
-		  {
-			  ITable result = null;
-			  DataTable schema;
+		public override ITable GetTableFromDB(IDataProvider provider, string tableName)
+		{
+			ITable result = null;
+			DataTable schema;
 
-			  using (var scope = new AutomaticConnectionScope(provider))
-			  {
-				  var restrictions = new string[4] { null, null, tableName, null };
-				  schema = scope.Connection.GetSchema("Columns", restrictions); //case difference between default and the Npgsql provider
-			  }
+			using (var scope = new AutomaticConnectionScope(provider))
+			{
+				var restrictions = new string[4] { null, null, tableName, null };
+				schema = scope.Connection.GetSchema("Columns", restrictions); //case difference between default and the Npgsql provider
+			}
 
-			  if (schema.Rows.Count > 0)
-			  {
-				  result = new DatabaseTable(tableName, provider);
-				  foreach (DataRow dr in schema.Rows)
-				  {
-					  IColumn col = new DatabaseColumn(dr["column_name"].ToString(), result);
-					  col.DataType = GetDbType(dr["DATA_TYPE"].ToString()); //TODO: might have to derive this from obvervation of attributes
-					  col.IsNullable = dr["is_nullable"].ToString() == "YES";
+			if (schema.Rows.Count > 0)
+			{
+				result = new DatabaseTable(tableName, provider);
+				foreach (DataRow dr in schema.Rows)
+				{
+					IColumn col = new DatabaseColumn(dr["column_name"].ToString(), result);
+					col.DataType = GetDbType(dr["DATA_TYPE"].ToString()); //TODO: might have to derive this from obvervation of attributes
+					col.IsNullable = dr["is_nullable"].ToString() == "YES";
 
-					  string maxLength = dr["character_maximum_length"].ToString();
+					string maxLength = dr["character_maximum_length"].ToString();
 
-					  int iMax = 0;
-					  int.TryParse(maxLength, out iMax);
-					  col.MaxLength = iMax;
-					  result.Columns.Add(col);
-				  }
-			  }
+					int iMax = 0;
+					int.TryParse(maxLength, out iMax);
+					col.MaxLength = iMax;
+					result.Columns.Add(col);
+				}
+			}
 
-			  return result;
-		  }
-	 }
+			return result;
+		}
+
+		public override string BuildCreateTableStatement(ITable table)
+		{
+			return base.BuildCreateTableStatement(table);
+		}
+
+		public override string GenerateColumns(ITable table)
+		{
+			StringBuilder createSql = new StringBuilder();
+
+			foreach (IColumn col in table.Columns)
+			{
+				createSql.AppendFormat("\r\n  {0}{1},", col.QualifiedName, GenerateColumnAttributes(col));
+			}
+
+			if (table.HasPrimaryKey)
+				createSql.AppendFormat("PRIMARY KEY({0}),", table.PrimaryKey.QualifiedName);
+			
+			string columnSql = createSql.ToString();
+			return columnSql.Chop(",");
+		}
+	}
 }
